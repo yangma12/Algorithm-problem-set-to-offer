ce62fe419c23faeeca3ebb90b5a2e58248c375f1
yangma12 <2319874157@qq.com>
2023-09-05 00:09:37 +0800

    05题完成

--- JianZhi_offer/offer05.cpp
+++ JianZhi_offer/offer05.cpp
@@ -1,15 +1,29 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 
-class Solution {
+class Solution05 {
 public:
     string replaceSpace(string s) {
-
+        int pos = s.find(' ');
+        while(pos != std::string::npos){
+            //pos存在就替换
+            s.erase(pos,1);
+            s.insert(pos,"%20");
+            pos = s.find(' ');
+            
+        }
+        return s;
     }
 };
 
 
 int main(){
-    
+    cout << "start" << endl;
+    string s("We are happy.");
+    Solution05 sol;
+    string r = sol.replaceSpace(s);
+    cout << r << endl;
+    return 0;
 }
